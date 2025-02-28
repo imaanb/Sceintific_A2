3df6c293f0ed684f83b0e352775925c2ac6df762
Rinske <r.m.oskamp@gmail.com>
2025-02-28 14:28:56 +0100

    B

--- 2.2.py
+++ 2.2.py
@@ -19,8 +19,12 @@ def random_walk(N, max_iter = 1000):
     init = np.random.choice(N+1)
     grid[N, init] = 1
 
+    all_grids = [grid]
+
     pos_y = 0
     pos_x = np.random.choice(N+1)
+
+    walkers = [(pos_x,pos_y)]
     for i in range(max_iter):
         # dir = np.random.choice([(0,1), (0,-1), (1,0), (-1,0)])
         directions = [np.array([-1, 0]), 
@@ -34,17 +38,21 @@ def random_walk(N, max_iter = 1000):
             new_pos_y = 0
             new_pos_x = np.random.choice(N+1)
 
+        walkers.append((new_pos_x,new_pos_y))
+        
         if cluster_in_neigborhood(N, new_pos_x, new_pos_y, grid):
             grid[new_pos_y, new_pos_x] = 1
+
+        all_grids.append(grid)
+
         if np.min(grid) == 1:
-            return grid
+            return all_grids, walkers
         pos_x = new_pos_x
         pos_y = new_pos_y
-    return grid
+    return all_grids, walkers 
 
 
-
-def plot_grid(grid):
+def plot_final_grid(grid):
     rows, cols = grid.shape
 
     # Create figure
@@ -72,9 +80,25 @@ def plot_grid(grid):
 
     plt.show()
 
-N = 10
-rw1 = random_walk(N)
-rw2 = random_walk(N)
+def plot_walker(route):
+    x, y = zip(*route) 
+    
+    plt.figure(figsize=(8, 6))
+    plt.scatter(x, y, color='red', label='Punten') 
+    
+    # Draw arrows
+    for i in range(len(route) - 1):
+        plt.arrow(x[i], y[i], x[i+1] - x[i], y[i+1] - y[i], 
+                  head_width=0.1, head_length=0.1, fc='black', ec='black', alpha = 0.2)
+    
+    # plt.grid()
+    plt.show()
 
-plot_grid(rw1)
+N = 5
+grids, route = random_walk(N)
+
+plot_walker(route)
+# rw2 = random_walk(N)
+
+# plot_grid(rw1)
 # plot_grid(rw2)
